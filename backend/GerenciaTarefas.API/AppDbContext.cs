e78ddb84718c941360d162c0d79afec2dd977a28
Luis Henrique <luishenriqueangelini@gmail.com>
2026-02-28 18:36:03 -0300

    Arquivos automáticos do backend adicionados ao gitignore + Correção de nome da tabela Tarefas para tarefas no backend

--- backend/GerenciaTarefas.API/AppDbContext.cs
+++ backend/GerenciaTarefas.API/AppDbContext.cs
@@ -7,6 +7,6 @@ namespace GerenciaTarefas.API
     {
         public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
 
-        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Tarefa> tarefas { get; set; }
     }
 }
--- backend/GerenciaTarefas.API/Repository/TarefasRepository.cs
+++ backend/GerenciaTarefas.API/Repository/TarefasRepository.cs
@@ -16,11 +16,11 @@ namespace GerenciaTarefas.API.Repository
 
         public async Task<List<TarefaResumidaDTO>> ListAll()
         {
-            List<TarefaResumidaDTO> tarefas = await _context.Tarefas
+            List<TarefaResumidaDTO> tarefas = await _context.tarefas
                 .Select(t => new TarefaResumidaDTO
                 {
-                    Id = t.Id,
-                    Titulo = t.Titulo
+                    Id = t.id,
+                    Titulo = t.titulo
                 })
                 .ToListAsync();
 
@@ -28,27 +28,27 @@ namespace GerenciaTarefas.API.Repository
         }
         public async Task<Tarefa?> GetById(int id)
         {
-            Tarefa? tarefa = await _context.Tarefas.FindAsync(id);
+            Tarefa? tarefa = await _context.tarefas.FindAsync(id);
             return tarefa;
         }
 
         public async Task Create(Tarefa tarefa)
         {
-            _context.Tarefas.Add(tarefa);
+            _context.tarefas.Add(tarefa);
             await _context.SaveChangesAsync();
         }
 
         public async Task<bool> Update(int id, Tarefa tarefa)
         {
-            Tarefa? tarefaBanco = await _context.Tarefas.FindAsync(id);
+            Tarefa? tarefaBanco = await _context.tarefas.FindAsync(id);
 
             if (tarefaBanco == null)
                 return false;
 
-            tarefaBanco.Titulo = tarefa.Titulo;
-            tarefaBanco.Descricao = tarefa.Descricao;
-            tarefaBanco.Prioridade = tarefa.Prioridade;
-            tarefaBanco.Status = tarefa.Status;
+            tarefaBanco.titulo = tarefa.titulo;
+            tarefaBanco.descricao = tarefa.descricao;
+            tarefaBanco.prioridade = tarefa.prioridade;
+            tarefaBanco.status = tarefa.status;
 
             await _context.SaveChangesAsync();
             return true;
@@ -56,12 +56,12 @@ namespace GerenciaTarefas.API.Repository
 
         public async Task<bool> Delete(int id)
         {
-            Tarefa? tarefa = await _context.Tarefas.FindAsync(id);
+            Tarefa? tarefa = await _context.tarefas.FindAsync(id);
 
             if (tarefa == null)
                 return false;
 
-            _context.Tarefas.Remove(tarefa);
+            _context.tarefas.Remove(tarefa);
             await _context.SaveChangesAsync();
             return true;
         }
